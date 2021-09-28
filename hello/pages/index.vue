d380c71e959bc52f0ad72e53c31827e508d83865
ryota-sato07 <satour.0722@gmail.com>
2021-09-28 20:58:51 +0900

    [Add] file

--- hello/pages/index.vue
+++ hello/pages/index.vue
@@ -1,8 +1,14 @@
 <template>
   <section class="container">
-      <h1>Top Page</h1>
+      <h1 class="title">Top Page</h1>
       <hr>
       <router-link to="/users/hello">Hello Page</router-link>
       <router-link to="/users/other">Other Page</router-link>
   </section>
 </template>
+
+<style>
+.title{
+  font-family: 'Gemunu Libre', sans-serif;
+}
+</style>
