7cdd08edb959d47dc7de53ea6f368200ed6678bf
ryota-sato07 <satour.0722@gmail.com>
2021-09-30 20:25:25 +0900

    [Add] file

--- hello/pages/index.vue
+++ hello/pages/index.vue
@@ -1,11 +1,29 @@
 <template>
   <section class="container">
       <div>
-        <h1>Hello!</h1>
+        <ul>
+          <li v-for="user in users" :key="user.id">
+            {{ user.id }},{{ user.name }},{{ user.company.name }}
+          </li>
+        </ul>
       </div>
   </section>
 </template>
 
 <script>
+const axios = require('axios')
+let url = 'https://jsonplaceholder.typicode.com/usersxxx'
 
+export default {
+  asyncData({ params, error }) {
+    return axios.get(url)
+      .then((res) =>  {
+      return { users: res.data }
+      })
+      .catch((e => {
+        // console.log(e.response.status)
+        error({ statusCode: e.response.status, message: e.message })
+      }))
+  }
+}
 </script>
