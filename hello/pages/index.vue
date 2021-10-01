b927cf2d1c988f35d22649d93fe54198401fd2c2
ryota-sato07 <satour.0722@gmail.com>
2021-10-01 10:34:05 +0900

    [Add] file

--- hello/pages/index.vue
+++ hello/pages/index.vue
@@ -1,29 +1,22 @@
 <template>
   <section class="container">
       <div>
-        <ul>
-          <li v-for="user in users" :key="user.id">
-            {{ user.id }},{{ user.name }},{{ user.company.name }}
-          </li>
-        </ul>
+        <Counter></Counter>
+        <hr>
+        <Counter></Counter>
+        <hr>
+        <Counter></Counter>
+        <hr>
       </div>
   </section>
 </template>
 
 <script>
-const axios = require('axios')
-let url = 'https://jsonplaceholder.typicode.com/usersxxx'
+import Counter from '~/components/Counter.vue'
 
 export default {
-  asyncData({ params, error }) {
-    return axios.get(url)
-      .then((res) =>  {
-      return { users: res.data }
-      })
-      .catch((e => {
-        // console.log(e.response.status)
-        error({ statusCode: e.response.status, message: e.message })
-      }))
+  components: {
+    Counter
   }
 }
 </script>
